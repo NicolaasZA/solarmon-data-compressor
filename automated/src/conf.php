f22f1dea1add18b341ca29de3159e5db4906e1af
NicolaasZA <lpretorius1995@gmail.com>
2022-06-08 14:13:12 +0200

    add gitignore

--- automated/src/conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class DBConfig
-{
-    public const DSN = "mysql:host=maverick.aserv.co.za;dbname=klausnij_nodb";
-    public const USR = "klausnij_nicolaas";
-    public const PWD = "fMpXY44rDp3n";
-}
